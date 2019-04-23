dffc8e4dc5dcf0df1b55f6ba0ac58f2a9d7ab972
error521 <42939219+error521@users.noreply.github.com>
2019-04-23 14:09:18 +0800

    Added more system testing
    (Copied from Telegram)
    
    # phase 1. Test that goes from 1. Trying to login, invalid username due to to typo, get error message, close error message, login successful,
    # phase 2. create ticket, check if ticket has been showed in dashboard
    # phase 3. clicks on ticket and enetr forum, send 2 types of messages, one pure text and one is with file link
    DONE UP TO HERE ^
    
    TODO:
    # phase 4. user logs out and admin now logs in
        # pahse 5. admin clicks on that ticket and enters the chat and sends back a response
    # phase 6. user to login and quick serach ticket number
    # phase 7. user should see the new message from admin

--- Source/website/Profile/tests/test_system.py
+++ Source/website/Profile/tests/test_system.py
@@ -18,29 +18,48 @@ class EditProfileByUser(LiveServerTestCase):
         self.driver.quit()
         super(EditProfileByUser, self).tearDown()
 
+    def is_text_present(self, text):
+        return str(text) in self.driver.page_source
+
     def test_EditProfileByUser(self):
         user_username = 'Tired'
         user_password = '1234'
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/login/")
+        driver.get("http://127.0.0.1:8000/")
         username_field = driver.find_element_by_name("username")
         password_field = driver.find_element_by_name("password")
         username_field.send_keys(user_username)
         time.sleep(1)
         password_field.send_keys(user_password)
         time.sleep(1)
-        login_button = driver.find_element_by_xpath("/html/body/form/div/button")
+        login_button = driver.find_element_by_xpath("/html/body/form/div/div/div/div/div/button")
         login_button.click()
         time.sleep(2)
         print("User has logged in")
-        time.sleep(1)
-        driver.get("http://127.0.0.1:8000/Profile/")
+
+        link_profile= driver.find_element_by_link_text('Profile')
+        link_profile.click()
         time.sleep(2)
-        phoneNumber_field = driver.find_element_by_name("phoneNumber")
+
+        update_profile_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/header/div/div/div/div/div/div/button")
         time.sleep(1)
-        phoneNumber_field.send_keys('98765432')
+        update_profile_button.click()
+
+        username_update = driver.find_element_by_xpath("/html/body/div[1]/div/div/header/div/div/div/div/div/div/div[2]/div/div/div[2]/form/div[1]/input")
+        time.sleep(2)
+        username_update.clear()
+        username_update.send_keys("Awake")
+        time.sleep(2)
+        submit_detail_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/header/div/div/div/div/div/div/div[2]/div/div/div[2]/form/div[8]/button[1]")
         time.sleep(1)
-        submit_new_button = driver.find_element_by_css_selector(".btn")
-        submit_new_button.click()
-        time.sleep(1)
+        submit_detail_button.click()
+
+        expectedMessage = 'X\nEdit profile success'
+        message = driver.find_element_by_xpath("/html/body/div[1]/div/div/header/div/div[1]")
+        msg_text = message.text
+        self.assertTrue(message)
+        self.assertEqual(msg_text, expectedMessage)
+        time.sleep(2)
+
+
         driver.quit()
--- Source/website/ticket_creation/tests/test_system.py
+++ Source/website/ticket_creation/tests/test_system.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import  NoSuchElementException
 import time
+import os
 from ticket_creation.models import All_Tickets, Ticket, Ticket_Details
 
 
@@ -17,6 +18,9 @@ class CreateTicketByUser(LiveServerTestCase):
     def tearDown(self):
         self.driver.quit()
         super(CreateTicketByUser, self).tearDown()
+
+    def is_text_present(self, text):
+        return str(text) in self.driver.page_source
 
     def test_CreateTicketByUser(self):
         user_username= 'Tired'
@@ -40,7 +44,7 @@ class CreateTicketByUser(LiveServerTestCase):
         link_create_ticket.click()
         time.sleep(2)
 
-        ticket_title = 'Help'
+        ticket_title = 'Help Ticket 16'
         ticket_description = 'Send Help'
         # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -67,11 +71,55 @@ class CreateTicketByUser(LiveServerTestCase):
         #time.sleep(1)
         ticket_create_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/form/div/div/div/div/div/button")
         ticket_create_button.click()
+        time.sleep(3)
         expectedMessage = 'x\nTicket creation success'
         message = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div")
         msg_text = message.text
         self.assertTrue(message)
         self.assertEqual(msg_text, expectedMessage)
+
+        # driver.quit()
+
+        close_notif = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/button")
+        close_notif.click()
+        time.sleep(2)
+        link_create_ticket = driver.find_element_by_link_text('Dashboard')
+        link_create_ticket.click()
+        time.sleep(2)
+        link_new_ticket = driver.find_element_by_link_text(ticket_title)
+        link_new_ticket.click()
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        reply_message_field = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/div[1]/input")
+        reply_message = 'I really appreciate your reply on this matter'
+        reply_message_field.send_keys(reply_message)
+        submit_reply_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/button")
+        submit_reply_button.click()
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        check_reply_sent = self.is_text_present(reply_message)
+        if check_reply_sent:
+            print("Reply has been successfully posted")
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        message_attach = "Attached a file"
+        reply_message_field = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/div[1]/input")
+        reply_message_field.send_keys(message_attach)
+        attach_file_button = driver.find_element_by_name('file')
+        attach_file_button.send_keys(os.getcwd()+ "\\ticket_creation\\tests\\createticketbug01.jpg")
+        # attach_file_button.click()
+        time.sleep(3)
+        submit_reply_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/button")
+        submit_reply_button.click()
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        check_reply_sent = self.is_text_present(message_attach)
+        if check_reply_sent:
+            print("Reply with file attachment has been successfully posted")
+
         driver.quit()
 
 
@@ -133,95 +181,159 @@ class ResolveTicketByAdmin(LiveServerTestCase):
         user_username = 'joe'
         user_password = '1234'
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/login/")
+        driver.get("http://127.0.0.1:8000/")
         username_field = driver.find_element_by_name("username")
         password_field = driver.find_element_by_name("password")
         username_field.send_keys(user_username)
         time.sleep(1)
         password_field.send_keys(user_password)
         time.sleep(1)
-        login_button = driver.find_element_by_xpath("/html/body/form/div/button")
+        login_button = driver.find_element_by_xpath("/html/body/form/div/div/div/div/div/button")
         login_button.click()
         time.sleep(2)
         print("Admin has logged in")
 
         # Admin has logged in
         # Admin clicks View all tickets link to create ticket
-        link_create_ticket = driver.find_element_by_link_text('View all tickets')
-        time.sleep(1)
-        link_create_ticket.click()
-        time.sleep(1)
-        link_view_ticket = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td[3]/a")
-        previousResolve = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td[5]").text
-        id = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td[1]").text
-        time.sleep(1)
-        link_view_ticket.click()
-        time.sleep(1)
-        resolve_button = driver.find_element_by_xpath("/html/body/div[1]/a")
-        resolve_button.click()
-        time.sleep(1)
-        alert_obj = driver.switch_to.alert
-        msg = alert_obj.text
-        print("Alert shows following message: " + msg)
-        alert_obj.accept()
-        time.sleep(1)
-        # driver.get("http://127.0.0.1:8000/ticket_creation/display/")
-        # time.sleep(3)
-        newResolve = driver.find_element_by_xpath("/html/body/table/tbody/tr[{}]/td[5]".format(id)).text
-        self.assertNotEqual(previousResolve, newResolve)
 
+        time.sleep(1)
+        # link_view_ticket = driver.find_element_by_link_text("View Tickets")
+        # link_view_ticket.click()
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        ticket_link = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/div/table/tbody/tr[1]/td[3]/a")
+        previousResolve = driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div/div[3]/div/table/tbody/tr[1]/td[5]").text
+        id = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/div/table/tbody/tr[1]/td[1]").text
+        time.sleep(1)
+        ticket_link.click()
+        time.sleep(1)
+        resolve_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[1]/div/form/a")
+        resolve_button.click()
+        time.sleep(3)
+        alert = driver.switch_to.alert
+        message = alert.text
+        print(message)
+        # self.assertEqual(message,"Do you want to resolve this ticket?")
+        time.sleep(2)
+        alert.accept()
+        time.sleep(2)
+        driver.get("http://127.0.0.1:8000/home/")
+        time.sleep(3)
+        newResolve = driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div/div[3]/div/table/tbody/tr[{}]/td[5]".format(id)).text
+        self.assertNotEqual(previousResolve, newResolve)
         driver.quit()
 
-
-class CancelResolveTicketByAdmin(LiveServerTestCase):
+class SystemTestingWebApp(LiveServerTestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        super(CancelResolveTicketByAdmin, self).setUp()
+        super(SystemTestingWebApp, self).setUp()
 
     def tearDown(self):
         self.driver.quit()
-        super(CancelResolveTicketByAdmin, self).tearDown()
+        super(SystemTestingWebApp, self).tearDown()
 
-    def test_ResolveTicketByAdmin(self):
-        user_username = 'joe'
+    def is_text_present(self, text):
+        return str(text) in self.driver.page_source
+
+    def test_SystemTestingWebApp(self):
+        user_username= 'Tired'
         user_password = '1234'
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/login/")
+        driver.get("http://127.0.0.1:8000/")
         username_field = driver.find_element_by_name("username")
         password_field = driver.find_element_by_name("password")
         username_field.send_keys(user_username)
         time.sleep(1)
         password_field.send_keys(user_password)
         time.sleep(1)
-        login_button = driver.find_element_by_xpath("/html/body/form/div/button")
+        login_button = driver.find_element_by_xpath("/html/body/form/div/div/div/div/div/button")
         login_button.click()
         time.sleep(2)
-        print("Admin has logged in")
+        print("User has logged in")
 
-        # Admin has logged in
-        # Admin clicks View all tickets link to create ticket
-        link_create_ticket = driver.find_element_by_link_text('View all tickets')
-        time.sleep(1)
+        # User has logged in
+        # User clicks Create Ticket link to create ticket
+        link_create_ticket = driver.find_element_by_link_text('Create Ticket')
         link_create_ticket.click()
+        time.sleep(2)
+
+        ticket_title = 'Help Ticket 16'
+        ticket_description = 'Send Help'
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        ticket_title_field = driver.find_element_by_name("title")
+        ticket_title_field.send_keys(ticket_title)
         time.sleep(1)
-        link_view_ticket = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td[3]/a")
-        previousResolve = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td[5]").text
-        id = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td[1]").text
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        ticket_description_field = driver.find_element_by_id('description')
+        # wait = WebDriverWait(driver,10)
+        # wait.until(EC.visibility_of(ticket_description_field))
+        # wait.until(EC.element_to_be_clickable(ticket_description_field))
+
+        ticket_description_field.send_keys(ticket_description)
         time.sleep(1)
-        link_view_ticket.click()
-        time.sleep(1)
-        resolve_button = driver.find_element_by_xpath("/html/body/div[1]/a")
-        resolve_button.click()
-        time.sleep(1)
-        alert_obj = driver.switch_to.alert
-        msg = alert_obj.text
-        print("Alert shows following message: " + msg)
-        alert_obj.dismiss()
-        time.sleep(1)
-        driver.get("http://127.0.0.1:8000/ticket_creation/display/")
-        # time.sleep(3)
-        newResolve = driver.find_element_by_xpath("/html/body/table/tbody/tr[{}]/td[5]".format(id)).text
-        self.assertEqual(previousResolve, newResolve)
+
+
+
+        # wait = WebDriverWait(driver, 20)
+        # wait.until(EC.visibility_of_element_located((By.NAME, "description")))
+        # driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+
+        # ticket_description_field.click()
+        #ticket_description_field.send_keys(ticket_description)
+        #time.sleep(1)
+        ticket_create_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/form/div/div/div/div/div/button")
+        ticket_create_button.click()
+        time.sleep(3)
+        expectedMessage = 'x\nTicket creation success'
+        message = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div")
+        msg_text = message.text
+        self.assertTrue(message)
+        self.assertEqual(msg_text, expectedMessage)
+
+        # driver.quit()
+
+        close_notif = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/button")
+        close_notif.click()
+        time.sleep(2)
+        link_create_ticket = driver.find_element_by_link_text('Dashboard')
+        link_create_ticket.click()
+        time.sleep(2)
+        link_new_ticket = driver.find_element_by_link_text(ticket_title)
+        link_new_ticket.click()
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        reply_message_field = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/div[1]/input")
+        reply_message = 'I really appreciate your reply on this matter'
+        reply_message_field.send_keys(reply_message)
+        submit_reply_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/button")
+        submit_reply_button.click()
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        check_reply_sent = self.is_text_present(reply_message)
+        if check_reply_sent:
+            print("Reply has been successfully posted")
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        message_attach = "Attached a file"
+        reply_message_field = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/div[1]/input")
+        reply_message_field.send_keys(message_attach)
+        attach_file_button = driver.find_element_by_name('file')
+        attach_file_button.send_keys(os.getcwd()+ "\\ticket_creation\\tests\\createticketbug01.jpg")
+        # attach_file_button.click()
+        time.sleep(3)
+        submit_reply_button = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/div/div/div/button")
+        submit_reply_button.click()
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        check_reply_sent = self.is_text_present(message_attach)
+        if check_reply_sent:
+            print("Reply with file attachment has been successfully posted")
 
         driver.quit()
+
 
